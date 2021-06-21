b98d8c3690ff4a31ed65ded2b64d6a86daef2714
Jean Oliveira Miranda - JMN <jean.miranda@iteris.com.br>
2021-06-21 18:49:37 -0300

    Autenticação com Next Auth

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,18 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
+
 import styles from "./styles.module.scss";
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.singInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.singInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      SenhorBiscoito
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.singInButton}>
+    <button
+      type="button"
+      className={styles.singInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
